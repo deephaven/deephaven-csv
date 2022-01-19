8a3776af1c46ca890205b642bcc74390e42eb8c1
Corey Kosak <coreykosak@deephaven.io>
2022-01-19 12:25:39 -0800

    Initial source

---
+++ src/main/java/io/deephaven/csv/parsers/TimestampMicrosParser.java
@@ -0,0 +1,10 @@
+package io.deephaven.csv.parsers;
+
+/** The parser for "microseconds since Unix epoch". */
+public class TimestampMicrosParser extends TimestampParserBase {
+    public static final TimestampMicrosParser INSTANCE = new TimestampMicrosParser();
+
+    private TimestampMicrosParser() {
+        super(MICROSECOND_SCALE);
+    }
+}
---
+++ src/main/java/io/deephaven/csv/parsers/TimestampNanosParser.java
@@ -0,0 +1,10 @@
+package io.deephaven.csv.parsers;
+
+/** The parser for "nanoseconds since Unix epoch". */
+public class TimestampNanosParser extends TimestampParserBase {
+    public static final TimestampNanosParser INSTANCE = new TimestampNanosParser();
+
+    private TimestampNanosParser() {
+        super(NANOSECOND_SCALE);
+    }
+}
