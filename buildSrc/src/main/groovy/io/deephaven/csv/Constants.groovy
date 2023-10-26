bfa6b670eebca69caa60e9ff3f5bf41e41aac656
Devin Smith <devinsmith@deephaven.io>
2023-10-26 12:52:27 -0700

    Add Java 21 testing, make Java 21 the default (#150)

--- buildSrc/src/main/groovy/io/deephaven/csv/Constants.groovy
+++ buildSrc/src/main/groovy/io/deephaven/csv/Constants.groovy
@@ -1,7 +1,7 @@
 package io.deephaven.csv
 
 class Constants {
-    public static int LATEST_LTS = 17
+    public static int LATEST_LTS = 21
 
-    public static int[] TEST_VERSIONS = [8, 11, 17, 19]
+    public static int[] TEST_VERSIONS = [8, 11, 17, 21]
 }
