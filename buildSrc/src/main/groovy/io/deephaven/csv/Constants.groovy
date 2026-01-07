ce68f0f95a4d4fb1a80614dd6352aaaf4a6f0548
Devin Smith <devinsmith@deephaven.io>
2026-01-07 14:53:46 -0800

    feat: Add Java 25 testing

--- buildSrc/src/main/groovy/io/deephaven/csv/Constants.groovy
+++ buildSrc/src/main/groovy/io/deephaven/csv/Constants.groovy
@@ -1,7 +1,7 @@
 package io.deephaven.csv
 
 class Constants {
-    public static int LATEST_LTS = 21
+    public static int LATEST_LTS = 25
 
-    public static int[] TEST_VERSIONS = [8, 11, 17, 21, 23]
+    public static int[] TEST_VERSIONS = [8, 11, 17, 21, 25]
 }
